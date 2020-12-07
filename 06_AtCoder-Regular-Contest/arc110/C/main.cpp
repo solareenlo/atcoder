a13ba91f4e84732508c9c38dc18a324dd24b649d
solareenlo <solareenlo@gmail.com>
2020-12-08 08:37:38 +0900

    Add: arc110_c

--- 06_AtCoder-Regular-Contest/arc110/C/main.cpp
+++ 06_AtCoder-Regular-Contest/arc110/C/main.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
-using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
 #define REP(i, n) for (int i = 0; i < (n); i++)
-using ll = long long;
+using namespace std;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
+	int n; cin >> n;
+	vector<int> p(n), pos(n), res;
+	REP(i, n) cin >> p[i], pos[--p[i]] = i;
 
+	vector<bool> used(n-1, false);
+	REP(i, n) {
+		int now = pos[i];
+		while (i < now) {
+			int before = now - 1;
+			if (used[before]) { cout << -1 << '\n'; return 0; }
+			used[before] = true;
+			pos[p[now]] = before;
+			pos[p[before]] = now;
+			swap(p[before], p[now]);
+			res.emplace_back(before);
+			now--;
+		}
+	}
+
+	if ((int)res.size() != n-1) cout << -1 << '\n';
+	else REP(i, n-1) cout << res[i] + 1 << '\n';
     return 0;
 }
