b0a74aa6e3b41fac7f315953ac8548caac855615
solareenlo <solareenlo@gmail.com>
2020-12-10 08:14:15 +0900

    Add: arc097_d

--- 06_AtCoder-Regular-Contest/arc097/D/main.cpp
+++ 06_AtCoder-Regular-Contest/arc097/D/main.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std;
 #include <atcoder/all>
-using namespace atcoder;
 #define REP(i, n) for (int i = 0; i < (n); i++)
-using ll = long long;
+using namespace std;
+using namespace atcoder;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
+	int n, m; cin >> n >> m;
+	vector<int> p(n);
+	REP(i, n) cin >> p[i], p[i]--;
+	vector<int> x(m), y(m);
+	REP(i, m) cin >> x[i] >> y[i], x[i]--, y[i]--;
+	dsu d(n);
+	REP(i, m) d.merge(x[i], y[i]);
+	int res = 0;
+	REP(i, n) if (d.same(i, p[i])) res++;
+	cout << res << '\n';
     return 0;
 }
