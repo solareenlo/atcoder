e73c86d7e179f21d6df66d3063f9d2ea89fefd15
solareenlo <solareenlo@gmail.com>
2020-12-09 10:13:33 +0900

    Update: arc054_a

--- 06_AtCoder-Regular-Contest/arc054/A/main.cpp
+++ 06_AtCoder-Regular-Contest/arc054/A/main.cpp
@@ -1,27 +1,19 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
-    double l, x, y, s, d;
-    cin >> l >> x >> y >> s >> d;
-
-    double ans = 0;
-    if (s <= d) {
-        ans = (d - s) / (x + y);
-        double ans2 = (l - (d - s)) / (y - x);
-        if (ans2 > 0)
-            ans = min(ans, ans2);
-    } else {
-        ans = (l - (s - d)) / (x + y);
-        double ans2 = (s - d) / (y - x);
-        if (ans2 > 0)
-            ans = min(ans, ans2);
-    }
-
-    printf("%.10f\n", ans);
-    return 0;
+	double l, x, y, s, d; cin >> l >> x >> y >> s >> d;
+	double res;
+	if (s <= d) {
+		res = (d - s) / (x + y);
+		double res2 = (l - (d - s)) / (y - x);
+		if (res2 > 0) res = min(res, res2);
+	}
+	else {
+		res = (l - (s - d)) / (x + y);
+		double res2 = (s - d) / (y - x);
+		if (res2 > 0) res = min(res, res2);
+	}
+	printf("%.10f\n", res);
+	return 0;
 }
--- 06_AtCoder-Regular-Contest/arc054/README.md
+++ 06_AtCoder-Regular-Contest/arc054/README.md
@@ -1,1 +1,1 @@
-# [AtCoder Regular Contest 054](https://atcoder.jp/contests/arc054/tasks)
+# [AtCoder Regular Contest 054](https://atcoder.jp/contests/arc054)
