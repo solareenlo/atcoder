2f2bf3f13d7993c18626c8fab079be81ee5f6aa2
solareenlo <solareenlo@gmail.com>
2020-12-16 14:16:19 +0900

    Update: agc024_a

--- 07_AtCoder-Grand-Contest/agc024/A/main.cpp
+++ 07_AtCoder-Grand-Contest/agc024/A/main.cpp
@@ -1,17 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
-    ll a, b, c, k;
-    cin >> a >> b >> c >> k;
-
-    ll ans = 0;
-    if (k % 2) ans = b - a;
-    else ans = a - b;
-
-    cout << ans << '\n';
-    return 0;
+	int64_t a, b, c, k; cin >> a >> b >> c >> k;
+	cout << ((k%2) ? b-a : a-b) <<'\n';
+	return 0;
 }
