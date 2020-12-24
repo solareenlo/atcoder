cac1e16198fee9bbd8d6f8c61355508264941ac7
solareenlo <solareenlo@gmail.com>
2020-12-24 17:33:50 +0900

    Add: abc154_e

--- 05_AtCoder-Beginner-Contest/abc154/E/E_001.cpp
+++ 05_AtCoder-Beginner-Contest/abc154/E/E_001.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
+int dp[101][2][10];
 int main() {
 	string str; cin >> str;
 	int n = str.size();
 	int K; cin >> K;
-
-	int dp[n+1][2][K+1];
-	memset(dp, 0, sizeof(dp));
-
 	dp[0][0][0] = 1;
-	REP(i, n) REP(j, 2) {
+	REP(i, n) REP(j, 2) REP(k, K+1) {
 		int r = j ? 9 : str[i]-'0';
-		REP(k, K+1) REP(l, r+1) {
-			int c = k + !!l;
-			if (c <= K)
-				dp[i+1][j||l<r][c] += dp[i][j][k];
-		}
+		REP(l, r+1)
+			if (k+!!l <= K)
+				dp[i+1][j||l<r][k+!!l] += dp[i][j][k];
 	}
 	cout << dp[n][0][K] + dp[n][1][K] << '\n';
-	return 0;
+    return 0;
 }
