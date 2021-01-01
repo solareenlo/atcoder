ae8fd14f5f67574d26a9b95439b3e63174e76efa
solareenlo <solareenlo@gmail.com>
2021-01-02 01:42:12 +0900

    Update: abc021_b

--- 05_AtCoder-Beginner-Contest/abc021/B/main.cpp
+++ 05_AtCoder-Beginner-Contest/abc021/B/main.cpp
@@ -1,37 +1,19 @@
-#include <iostream>
-#include <set>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-const string YES = "YES";
-const string NO = "NO";
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
-
-    set<int> town;
-    int tmp;
-    cin >> tmp;
-    town.insert(tmp);
-    cin >> tmp;
-    town.insert(tmp);
-
-    int k;
-    cin >> k;
-    REP(i, k) {
-        cin >> tmp;
-        town.insert(tmp);
-    }
-
-    if (town.size() == 2 + k) print(YES);
-    else print(NO);
-    return 0;
+int main() {
+	int n; cin >> n;
+	set<int> town;
+	REP(i, 2) {
+		int a; cin >> a;
+		town.insert(a);
+	}
+	int k; cin >> k;
+	REP(i, k) {
+		int p; cin >> p;
+		town.insert(p);
+	}
+	cout << (((int)town.size() == k+2) ? "YES" : "NO") << '\n';
+	return 0;
 }
