5c5f25b7fc5678153d741f8a00cba43423df8ad5
solareenlo <solareenlo@gmail.com>
2021-01-04 05:36:49 +0900

    Add: arc021_b

--- 06_AtCoder-Regular-Contest/arc021/B/main.cpp
+++ 06_AtCoder-Regular-Contest/arc021/B/main.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
+	int n; cin >> n;
+	int b[n];
+	int x = 0;
+	REP(i, n) {
+		cin >> b[i];
+		x ^= b[i];
+	}
+	if (x != 0) {
+		cout << -1 << '\n';
+		return 0;
+	}
+	int res = 0;
+	REP(i, n) {
+		cout << res << '\n';
+		res ^= b[i];
+	}
     return 0;
 }
