f68d9a6a7a3754d6b531798e96590df2a5383cc6
solareenlo <solareenlo@gmail.com>
2021-01-05 04:18:23 +0900

    Add: arc018_b

--- 06_AtCoder-Regular-Contest/arc018/B/main.cpp
+++ 06_AtCoder-Regular-Contest/arc018/B/main.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
+	int n; cin >> n;
 
+	int64_t x[n], y[n];
+	REP(i, n) cin >> x[i] >> y[i];
+
+	int cnt = 0;
+	REP(i, n) REP(j, i) REP(k, j) {
+		int64_t x1 = x[j] - x[i];
+		int64_t x2 = x[k] - x[i];
+		int64_t y1 = y[j] - y[i];
+		int64_t y2 = y[k] - y[i];
+		int64_t s = x1 * y2 - x2 * y1;
+		if (s != 0 && s % 2 == 0) cnt++;
+	}
+	cout << cnt << '\n';
     return 0;
 }
