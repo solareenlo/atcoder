9fee27cbb2f52953f258553b8e5a7bc28e54aedb
solareenlo <solareenlo@gmail.com>
2021-01-15 12:56:13 +0900

    Add: abc188_e

--- 05_AtCoder-Beginner-Contest/abc188/E/E_001.cpp
+++ 05_AtCoder-Beginner-Contest/abc188/E/E_001.cpp
@@ -8,16 +8,16 @@ int main() {
 	vector<int> G[n];
 	while (m--) {
 		int x, y; cin >> x >> y;
-		G[--x].push_back(--y);
+		G[x-1].push_back(y-1);
 	}
 	vector<int> dp(n, 2e9);
 	int maxi = -2e9;
 	for (int i = 0; i < n; i++) {
+		maxi = max(maxi, a[i] - dp[i]);
+		dp[i] = min(dp[i], a[i]);
 		for (int j : G[i])
-			dp[j] = min(dp[j], min(dp[i], a[i]));
-		if (dp[i] != 2e9)
-			maxi = max(maxi, a[i] - dp[i]);
+			dp[j] = min(dp[j], dp[i]);
 	}
 	cout << maxi << '\n';
-	return 0;
+    return 0;
 }
