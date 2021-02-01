8f9581367810b063d8a55d8500dd3a084757495c
solareenlo <solareenlo@gmail.com>
2021-02-01 22:27:51 +0900

    Add: abc190_d

--- 05_AtCoder-Beginner-Contest/abc190/D/main.cpp
+++ 05_AtCoder-Beginner-Contest/abc190/D/main.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
-#define REP(i, n) for (int i = 0; i < (n); i++)
-using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
+	int64_t	n; cin >> n;
+	n*=2;
+	int64_t	cnt=0;
+	for (int64_t i=1; i*i<=n; i++)
+		if (n%i==0 && (i%2||(n/i)%2))
+			cnt++;
+	cout << cnt*2 << '\n';
     return 0;
 }
