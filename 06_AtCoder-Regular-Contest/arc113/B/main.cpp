74f53b46766e45adbe877986464a0a6c78ab9d0e
solareenlo <solareenlo@gmail.com>
2021-02-23 06:21:35 +0900

    Add: arc113_b

--- 06_AtCoder-Regular-Contest/arc113/B/main.cpp
+++ 06_AtCoder-Regular-Contest/arc113/B/main.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 
 int main() {
+	int a, b, c; cin >> a >> b >> c;
+	cout << pow_mod(a, pow_mod(b, c, 4)+4, 10) << '\n';
     return 0;
 }
