762e2bffd35936d38ad6fae3580d4817befa2a0b
solareenlo <solareenlo@gmail.com>
2021-03-09 04:49:28 +0900

    Add: abc194_d

--- 05_AtCoder-Beginner-Contest/abc194/D/D_001.cpp
+++ 05_AtCoder-Beginner-Contest/abc194/D/D_001.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	int n; cin >> n;
+	int	n; cin >> n;
 	double res = 0.0;
 	for (int i=1; i<n; i++)
-		res += 1.0/i;
-	printf("%.10f\n", res*n);
-	return 0;
+		res += 1.0*n/(n-i);
+	printf("%.10f\n", res);
+    return 0;
 }
