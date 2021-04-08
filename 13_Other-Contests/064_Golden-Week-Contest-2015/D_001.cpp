3488d1394fe74be61301f51e574758fe63a5509a
solareenlo <solareenlo@gmail.com>
2021-04-08 15:25:57 +0900

    Add: Golen-Week-Contest-2015_d

---
+++ 13_Other-Contests/064_Golden-Week-Contest-2015/D_001.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i=0; i<n; i++)
+using namespace std;
+
+int main() {
+	int h, w, k; cin >> h >> w >> k;
+	int v[h][w];
+	REP(i, h) REP(j, w) {
+		printf("1 1 %d %d\n", i+1, j+1);
+		cin >> v[i][j];
+	}
+	while (k--) {
+		int a, b, c, d; cin >> a >> b >> c >> d;
+		a--, b--, c--, d--;
+		if (b>d || (b==d && a<b)) {
+			swap(a, c);
+			swap(b, d);
+		}
+		if (a<=c && b<=d)
+			cout << v[c][d] - v[a][b] << '\n';
+		else
+			cout << v[a][b] - v[c][b] - v[c][b] + v[c][d] << '\n';
+	}
+	return 0;
+}
