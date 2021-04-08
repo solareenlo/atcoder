e249ccc4e384e74c95683f5a59573dc9918351a3
solareenlo <solareenlo@gmail.com>
2021-04-09 01:18:35 +0900

    Add: Golen-Week-Contest-2015_e

---
+++ 13_Other-Contests/064_Golden-Week-Contest-2015/E_001.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+mint f[2000001];
+int main() {
+	int n, k, l; cin >> n >> k >> l;
+	f[0] = 1;
+	for (int i=1; i<=2000000; i++)
+		f[i] = f[i-1]*i;
+	mint a = 0, K = k, N = n;
+	if (n==l) {
+		for (int i=0; i<n; i++)
+			a += K.pow(gcd(n, i));
+		a *= N.inv();
+	} else {
+		a = f[n+k-1]*f[n].inv()*f[k-1].inv();
+		if (l%2 && n<=k)
+			a += f[k]*f[n].inv()*f[k-n].inv();
+	}
+	cout << a.val() << '\n';
+	return 0;
+}
