6442b9fdf34fddbfd5df4696c1e3948f81324fd3
solareenlo <solareenlo@gmail.com>
2021-05-07 02:16:34 +0900

    fix

--- 13_Other-Contests/074_IOIer-Japan-PC-2015/A_001.cpp
+++ 13_Other-Contests/074_IOIer-Japan-PC-2015/A_001.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int64_t n, x, sum, maxi;
+int64_t n, a, sum, maxi;
 int main() {
 	cin >> n;
-	while (cin >> x) {
-		sum += x;
-		maxi = max(maxi, x);
+	while (cin >> a) {
+		sum += a;
+		maxi = max(maxi, a);
 	}
-	cout << sum + maxi + n<< '\n';
+	cout << sum + maxi + n << '\n';
 	return 0;
 }
