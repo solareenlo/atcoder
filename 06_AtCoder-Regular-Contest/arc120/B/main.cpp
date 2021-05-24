dc4398de32b3a9aa307736cc39ce03f52f90f2cf
solareenlo <solareenlo@gmail.com>
2021-05-25 02:04:54 +0900

    Add: arc120_b

--- 06_AtCoder-Regular-Contest/arc120/B/main.cpp
+++ 06_AtCoder-Regular-Contest/arc120/B/main.cpp
@@ -1,7 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #define REP(i, n) for (int i = 0; i < (n); i++)
-using namespace std;
-
+char s[510][510];
+int r[1010], b[1010];
 int main() {
+    int n, m; std::cin >> n >> m;
+    REP(i, n) {
+        std::cin >> s[i];
+        REP(j, m) {
+            if (s[i][j] == 'R')
+                r[i+j] = 1;
+            if (s[i][j] == 'B')
+                b[i+j] = 1;
+        }
+    }
+    int res = 1;
+    REP(i, n+m-1)
+        res = res*(2-r[i]-b[i])%998244353;
+    std::cout << res << '\n';
     return 0;
 }
