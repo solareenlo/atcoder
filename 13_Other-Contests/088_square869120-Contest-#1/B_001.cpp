797f617e9ceef59166c2e1518b8242d371ceeb6e
solareenlo <solareenlo@gmail.com>
2021-06-02 04:19:19 +0900

    Add: square869120-Contest-#1_b

---
+++ 13_Other-Contests/088_square869120-Contest-#1/B_001.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+    int h, w, n; std::cin >> h >> w >> n;
+    if (n % 2) {
+        std::cout << -1 << '\n';
+    } else {
+        double v[n];
+        for (int i = 0; i < n; i++) {
+            int x, y; std::cin >> x >> y;
+            v[i] = (1.*x)/y;
+        }
+        std::sort(v, v+n);
+        bool flag = false;
+        for (int i = 1; i <= w; i++) {
+            for (int j = 1; j <= h; j++) {
+                if (i == w || j == h) {
+                    if (v[n/2-1]<1.*i/j && v[n/2]>1.*i/j) {
+                        printf("(%d,%d)\n", i, j);
+                        flag = true;
+                    }
+                }
+            }
+        }
+        if (!flag)
+            std::cout << -1 << std::endl;
+    }
+    return 0;
+}
