50609ddd4fb62079a540db70c3a627030600afc1
solareenlo <solareenlo@gmail.com>
2021-06-08 03:58:16 +0900

    Add: abc204_d

--- 05_AtCoder-Beginner-Contest/abc204/D/main.cpp
+++ 05_AtCoder-Beginner-Contest/abc204/D/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <algorithm>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
+int t[1001], dp[200001];
+
 int main() {
+    int n; std::cin >> n;
+    int sum = 0;
+    REP(i, n) {
+        std::cin >> t[i];
+        sum += t[i];
+    }
+    REP(i, n) for (int j = sum/2; j >= t[i]; j--)
+        dp[j] = std::max(dp[j], dp[j-t[i]] + t[i]);
+    std::cout << sum - dp[sum/2] << std::endl;
     return 0;
 }
