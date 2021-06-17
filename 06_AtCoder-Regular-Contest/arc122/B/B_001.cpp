b3831c6c99ad10aa2cc8cc4083ef8c99ac404545
solareenlo <solareenlo@gmail.com>
2021-06-17 19:16:12 +0900

    Add: arc122_b

--- 06_AtCoder-Regular-Contest/arc122/B/B_001.cpp
+++ 06_AtCoder-Regular-Contest/arc122/B/B_001.cpp
@@ -1,20 +1,20 @@
 // Copyright
 
-#include <algorithm>
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
     int n; std::cin >> n;
-    int a[n];
+    double a[n];
     REP(i, n)
         std::cin >> a[i];
     std::sort(a, a+n);
-    int num = a[n/2];
-    int64_t res = 0;
+    double x = a[n/2] / 2;
+    double res = 0;
     REP(i, n)
-        res += a[i] - std::min(a[i], num);
-    std::cout << std::setprecision(10) << 1.0*res/n + 1.0*num/2 << std::endl;
+        res += (x+a[i]-std::min(a[i], x*2))/n;
+    std::cout << std::setprecision(10) << res << std::endl;
     return 0;
 }
