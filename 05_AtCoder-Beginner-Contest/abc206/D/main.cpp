f683f0b210a1700f18ab9ebcd20f8b7eb63d581b
solareenlo <solareenlo@gmail.com>
2021-06-21 02:34:24 +0900

    Add: abc206_d

--- 05_AtCoder-Beginner-Contest/abc206/D/main.cpp
+++ 05_AtCoder-Beginner-Contest/abc206/D/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <atcoder/all>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
+    int n; std::cin >> n;
+    int a[n];
+    REP(i, n)
+        std::cin >> a[i];
+    int res = 0;
+    atcoder::dsu uf(200001); // Union Find
+    REP(i, n/2) {
+        int l = a[i], r = a[n-1-i];
+        res += !uf.same(l, r);
+        uf.merge(l, r);
+    }
+    std::cout << res << std::endl;
     return 0;
 }
